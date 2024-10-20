3479d853ac0c500de4d0ad7462174e679e63eeb9
agaesh <agaeshk@gmail.com>
2024-10-20 18:44:56 +0800

    added product page to add items in the database

---
+++ product/add.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="productName" placeholder="Enter Product Name">
+        <input type="text" name = "Descrption" placeholder = "Enter Product Descrption">
+        <input type="text" name = "Quantity" placeholder = "Quantity">
+        <input type="text" name = "price" placeholder = "Price">
+        <textarea name="remark" name = "submit" id="">
+        </textarea>
+        <input type="submit" value="Add Product">
+    </form>
+</body>
+</html>
