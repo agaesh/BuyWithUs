dd48189577bbc2f9d6b399bbdb96c1d9fe8d0f9a
agaesh <agaeshk@gmail.com>
2024-11-02 13:22:15 +0800

    handled basic design for the admin and seller register/login page

--- index.php
+++ index.php
@@ -7,6 +7,66 @@
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <style>
+        *{
+            font-family: 'montserrat',sans-serif;
+            padding:0;
+            margin:0;
+            box-sizing: border-box;
+        }
+        .header nav a{
+            text-decoration: none;
+            color:white;
+            margin : 0 5px;
+
+        }
+        input,select{
+            padding:10px;
+            border:1px solid #ccc;
+            outline:none;
+            width:300px;
+            margin:10px;
+            border-radius:4px;
+        }
+        .header{
+            width:100%;
+        }
+        .header nav{
+            display:flex;
+            flex-direction:Row;
+            background-color: red;
+            align-items: center;
+            height:30px;
+            padding:30px;
+            background-color: #111184;
+            color:yellow;
+        }
+        .header nav .logo h2{
+            font-size: 19px;
+        }
+        .header nav .menu-link {
+            display:inline-flex;
+            margin-left: auto;
+            min-width: 100px;
+        }
+        .header nav li{
+            list-style: none;
+        }
+        .container{
+            display:flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+        form{
+            display:flex;
+            flex-direction: column;
+        }
+        input[type = "submit"]{
+            border:none;
+            outline:none;
+            font-weight: bolder;
+            background-color: gray;
+        }
 <body>
     <?php
       if(isset($_REQUEST['REQUEST_METHOD'])){
@@ -46,7 +106,7 @@
                 </option>
             </select>
             <a href="" style = "color:black; text-decoration:underline; text-align:Center;">Forgot Password?</a>
-            <input type="submit">
+            <input type="submit" value = "Register">
         </form>
     </div>
 </body>
